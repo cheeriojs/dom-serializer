12cc3aeef2690a81691d165f3adbc94f816d0c85
Felix Böhm <me@feedic.com>
2020-05-09 00:54:55 -0700

    Bump dependencies, convert to TS

---
+++ src/index.test.ts
@@ -0,0 +1,198 @@
+import cheerio from "cheerio";
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import parse from "cheerio/lib/parse";
+import render from "./index";
+
+const defaultOpts = cheerio.prototype.options;
+
+type CheerioOptions = {
+  _useHtmlParser2?: boolean;
+  normalizeWhitespace?: boolean;
+  decodeEntities?: boolean;
+};
+
+function html(
+  preset: CheerioOptions,
+  str: string,
+  options: CheerioOptions = {}
+) {
+  const opts = { ...defaultOpts, ...preset, ...options };
+  const dom = parse(str, opts);
+  return render(dom, opts);
+}
+
+function xml(str: string, options: CheerioOptions = {}) {
+  const opts = { ...defaultOpts, ...options, xmlMode: true };
+  const dom = parse(str, opts);
+  return render(dom, opts);
+}
+
+describe("render DOM parsed with htmlparser2", () => {
+  // only test applicable to the default setup
+  describe("(html)", () => {
+    const htmlFunc = html.bind(null, { _useHtmlParser2: true });
+    // it doesn't really make sense for {decodeEntities: false}
+    // since currently it will convert <hr class='blah'> into <hr class="blah"> anyway.
+    it("should handle double quotes within single quoted attributes properly", () => {
+      const str = "<hr class='an \"edge\" case' />";
+      expect(htmlFunc(str)).toStrictEqual(
+        '<hr class="an &quot;edge&quot; case">'
+      );
+    });
+  });
+
+  // run html with default options
+  describe(
+    "(html, {})",
+    testBody.bind(null, html.bind(null, { _useHtmlParser2: true }))
+  );
+
+  // run html with turned off decodeEntities
+  describe(
+    "(html, {decodeEntities: false})",
+    testBody.bind(
+      null,
+      html.bind(null, { _useHtmlParser2: true, decodeEntities: false })
+    )
+  );
+
+  describe("(xml)", () => {
+    it("should render CDATA correctly", () => {
+      const str =
+        "<a> <b> <![CDATA[ asdf&asdf ]]> <c/> <![CDATA[ asdf&asdf ]]> </b> </a>";
+      expect(xml(str)).toStrictEqual(str);
+    });
+
+    it('should append ="" to attributes with no value', () => {
+      const str = "<div dropdown-toggle>";
+      expect(xml(str)).toStrictEqual('<div dropdown-toggle=""/>');
+    });
+
+    it('should append ="" to boolean attributes with no value', () => {
+      const str = "<input disabled>";
+      expect(xml(str)).toStrictEqual('<input disabled=""/>');
+    });
+
+    it("should preserve XML prefixes on attributes", () => {
+      const str =
+        '<div xmlns:ex="http://example.com/ns"><p ex:ample="attribute">text</p></div>';
+      expect(xml(str)).toStrictEqual(str);
+    });
+
+    it("should preserve mixed-case XML elements and attributes", () => {
+      const str = '<svg viewBox="0 0 8 8"><radialGradient/></svg>';
+      expect(xml(str)).toStrictEqual(str);
+    });
+  });
+});
+
+function testBody(html: (input: string, opts?: CheerioOptions) => string) {
+  it("should render <br /> tags correctly", () => {
+    const str = "<br />";
+    expect(html(str)).toStrictEqual("<br>");
+  });
+
+  it("should retain encoded HTML content within attributes", () => {
+    const str = '<hr class="cheerio &amp; node = happy parsing" />';
+    expect(html(str)).toStrictEqual(
+      '<hr class="cheerio &amp; node = happy parsing">'
+    );
+  });
+
+  it('should shorten the "checked" attribute when it contains the value "checked"', () => {
+    const str = "<input checked/>";
+    expect(html(str)).toStrictEqual("<input checked>");
+  });
+
+  it('should not shorten the "name" attribute when it contains the value "name"', () => {
+    const str = '<input name="name"/>';
+    expect(html(str)).toStrictEqual('<input name="name">');
+  });
+
+  it('should not append ="" to attributes with no value', () => {
+    const str = "<div dropdown-toggle>";
+    expect(html(str)).toStrictEqual("<div dropdown-toggle></div>");
+  });
+
+  it("should render comments correctly", () => {
+    const str = "<!-- comment -->";
+    expect(html(str)).toStrictEqual("<!-- comment -->");
+  });
+
+  it("should render whitespace by default", () => {
+    const str = '<a href="./haha.html">hi</a> <a href="./blah.html">blah</a>';
+    expect(html(str)).toStrictEqual(str);
+  });
+
+  it("should normalize whitespace if specified", () => {
+    const str = '<a href="./haha.html">hi</a> <a href="./blah.html">blah  </a>';
+    expect(html(str, { normalizeWhitespace: true })).toStrictEqual(
+      '<a href="./haha.html">hi</a> <a href="./blah.html">blah </a>'
+    );
+  });
+
+  it("should preserve multiple hyphens in data attributes", () => {
+    const str = '<div data-foo-bar-baz="value"></div>';
+    expect(html(str)).toStrictEqual('<div data-foo-bar-baz="value"></div>');
+  });
+
+  it("should not encode characters in script tag", () => {
+    const str = '<script>alert("hello world")</script>';
+    expect(html(str)).toStrictEqual(str);
+  });
+
+  it("should not encode json data", () => {
+    const str =
+      '<script>var json = {"simple_value": "value", "value_with_tokens": "&quot;here & \'there\'&quot;"};</script>';
+    expect(html(str)).toStrictEqual(str);
+  });
+
+  it("should render childless SVG nodes with a closing slash in HTML mode", () => {
+    const str =
+      '<svg><circle x="12" y="12"/><path d="123M"/><polygon points="60,20 100,40 100,80 60,100 20,80 20,40"/></svg>';
+    expect(html(str)).toStrictEqual(str);
+  });
+
+  it("should render childless MathML nodes with a closing slash in HTML mode", () => {
+    const str = "<math><infinity/></math>";
+    expect(html(str)).toStrictEqual(str);
+  });
+
+  it("should allow SVG elements to have children", () => {
+    const str = '<svg><circle cx="12" r="12"><title>dot</title></circle></svg>';
+    expect(html(str)).toStrictEqual(str);
+  });
+
+  it("should not include extra whitespace in SVG self-closed elements", () => {
+    const str = '<svg><image href="x.png"/>     </svg>';
+    expect(html(str)).toStrictEqual(str);
+  });
+
+  it("should fix-up bad nesting in SVG in HTML mode", () => {
+    const str = '<svg><g><image href="x.png"></svg>';
+    expect(html(str)).toStrictEqual('<svg><g><image href="x.png"/></g></svg>');
+  });
+
+  it("should preserve XML prefixed attributes on inline SVG nodes in HTML mode", () => {
+    const str =
+      '<svg><text id="t" xml:lang="fr">Bonjour</text><use xlink:href="#t"/></svg>';
+    expect(html(str)).toStrictEqual(str);
+  });
+
+  it("should handle mixed-case SVG content in HTML mode", () => {
+    const str = '<svg viewBox="0 0 8 8"><radialGradient/></svg>';
+    expect(html(str)).toStrictEqual(str);
+  });
+
+  it("should render HTML content in SVG foreignObject in HTML mode", () => {
+    const str =
+      '<svg><foreignObject requiredFeatures=""><img src="test.png" viewbox>text<svg viewBox="0 0 8 8"><circle r="3"/></svg></foreignObject></svg>';
+    expect(html(str)).toStrictEqual(str);
+  });
+
+  it("should render iframe nodes with a closing tag in HTML mode", () => {
+    const str = '<iframe src="test"></iframe>';
+    expect(html(str)).toStrictEqual(str);
+  });
+}
---
+++ src/index.ts
@@ -0,0 +1,203 @@
+/*
+  Module dependencies
+*/
+import * as ElementType from "domelementtype";
+import { Node, NodeWithChildren, Element, DataNode } from "domhandler";
+import entities from "entities";
+
+/* mixed-case SVG and MathML tags & attributes
+   recognized by the HTML parser, see
+   https://html.spec.whatwg.org/multipage/parsing.html#parsing-main-inforeign
+*/
+import { elementNames, attributeNames } from "./foreignNames";
+
+export interface DomSerializerOptions {
+  xmlMode?: boolean | "foreign";
+  decodeEntities?: boolean;
+}
+
+const unencodedElements = new Set([
+  "style",
+  "script",
+  "xmp",
+  "iframe",
+  "noembed",
+  "noframes",
+  "plaintext",
+  "noscript",
+]);
+
+/*
+  Format attributes
+*/
+function formatAttributes(
+  attributes: Record<string, string> | undefined,
+  opts: DomSerializerOptions
+) {
+  if (!attributes) return;
+
+  return Object.keys(attributes)
+    .map((key) => {
+      const value = attributes[key];
+
+      if (opts.xmlMode === "foreign") {
+        /* fix up mixed-case attribute names */
+        key = attributeNames.get(key) ?? key;
+      }
+
+      if (!opts.xmlMode && (value === null || value === "")) {
+        return key;
+      }
+
+      return `${key}="${
+        opts.decodeEntities
+          ? entities.encodeXML(value)
+          : value.replace(/"/g, "&quot;")
+      }"`;
+    })
+    .join(" ");
+}
+
+/*
+  Self-enclosing tags
+*/
+const singleTag = new Set([
+  "area",
+  "base",
+  "basefont",
+  "br",
+  "col",
+  "command",
+  "embed",
+  "frame",
+  "hr",
+  "img",
+  "input",
+  "isindex",
+  "keygen",
+  "link",
+  "meta",
+  "param",
+  "source",
+  "track",
+  "wbr",
+]);
+
+/**
+ * Renders a DOM node or an array of DOM nodes to a string.
+ *
+ * Can be thought of as the equivalent of the `outerHTML` of the passed node(s).
+ *
+ * @param node Node to be rendered.
+ * @param options Changes serialization behavior
+ */
+export default function render(
+  node: Node | Node[],
+  options: DomSerializerOptions = {}
+): string {
+  // TODO: This is a bit ugly
+  const nodes: Node[] =
+    Array.isArray(node) || (node as any).cheerio ? (node as Node[]) : [node];
+
+  let output = "";
+
+  for (let i = 0; i < nodes.length; i++) {
+    output += renderNode(nodes[i], options);
+  }
+
+  return output;
+}
+
+function renderNode(node: Node, options: DomSerializerOptions): string {
+  switch (node.type as string) {
+    case "root":
+      return render((node as NodeWithChildren).children, options);
+    case ElementType.Directive:
+      return renderDirective(node as DataNode);
+    case ElementType.Comment:
+      return renderComment(node as DataNode);
+    case ElementType.CDATA:
+      return renderCdata(node as NodeWithChildren);
+    default:
+      return ElementType.isTag(node)
+        ? renderTag(node as Element, options)
+        : renderText(node as DataNode, options);
+  }
+}
+
+const foreignModeIntegrationPoints = new Set([
+  "mi",
+  "mo",
+  "mn",
+  "ms",
+  "mtext",
+  "annotation-xml",
+  "foreignObject",
+  "desc",
+  "title",
+]);
+
+function renderTag(elem: Element, opts: DomSerializerOptions) {
+  // Handle SVG / MathML in HTML
+  if (opts.xmlMode === "foreign") {
+    /* fix up mixed-case element names */
+    elem.name = elementNames.get(elem.name) ?? elem.name;
+    /* exit foreign mode at integration points */
+    if (
+      elem.parent &&
+      foreignModeIntegrationPoints.has((elem.parent as Element).name)
+    )
+      opts = { ...opts, xmlMode: false };
+  }
+  if (!opts.xmlMode && ["svg", "math"].indexOf(elem.name) >= 0) {
+    opts = { ...opts, xmlMode: "foreign" };
+  }
+
+  let tag = `<${elem.name}`;
+  const attribs = formatAttributes(elem.attribs, opts);
+
+  if (attribs) {
+    tag += ` ${attribs}`;
+  }
+
+  if (opts.xmlMode && (!elem.children || elem.children.length === 0)) {
+    tag += "/>";
+  } else {
+    tag += ">";
+    if (elem.children) {
+      tag += render(elem.children, opts);
+    }
+
+    if (!singleTag.has(elem.name) || opts.xmlMode) {
+      tag += `</${elem.name}>`;
+    }
+  }
+
+  return tag;
+}
+
+function renderDirective(elem: DataNode) {
+  return `<${elem.data}>`;
+}
+
+function renderText(elem: DataNode, opts: DomSerializerOptions) {
+  let data = elem.data || "";
+
+  // if entities weren't decoded, no need to encode them back
+  if (
+    opts.decodeEntities &&
+    !(elem.parent && unencodedElements.has((elem.parent as Element).name))
+  ) {
+    data = entities.encodeXML(data);
+  }
+
+  return data;
+}
+
+function renderCdata(elem: NodeWithChildren) {
+  return `<![CDATA[${(elem.children[0] as DataNode).data}]]>`;
+}
+
+function renderComment(elem: DataNode) {
+  return `<!--${elem.data}-->`;
+}
