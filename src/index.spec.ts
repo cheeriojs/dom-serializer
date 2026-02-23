a2368748053e302b5a4d818e788f767e7d1961ff
Felix <188768+fb55@users.noreply.github.com>
2026-02-23 21:51:50 +0000

    Update index.spec.ts

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -11,17 +11,17 @@ interface LoadingOptions extends CheerioOptions {
 
 function html(
   preset: LoadingOptions,
-  string_: string,
+  markup: string,
   options: LoadingOptions = {},
 ) {
   const options_ = { ...preset, ...options };
-  const $ = load(string_, options_, true);
+  const $ = load(markup, options_, true);
   return render($._root, options_);
 }
 
-function xml(string_: string, options: LoadingOptions = {}) {
+function xml(markup: string, options: LoadingOptions = {}) {
   const options_ = { ...options, xmlMode: true };
-  const $ = load(string_, options_, true);
+  const $ = load(markup, options_, true);
   return render($._root, options_);
 }
 
@@ -34,16 +34,16 @@ describe("render DOM parsed with htmlparser2", () => {
      * since currently it will convert <hr class='blah'> into <hr class="blah"> anyway.
      */
     it("should handle double quotes within single quoted attributes properly", () => {
-      const string_ = "<hr class='an \"edge\" case' />";
-      expect(htmlFunction(string_)).toStrictEqual(
+      const markup = "<hr class='an \"edge\" case' />";
+      expect(htmlFunction(markup)).toStrictEqual(
         '<hr class="an &quot;edge&quot; case">',
       );
     });
 
     it("should escape entities to utf8 if requested", () => {
-      const string_ = '<a href="a < b &quot; & c">& " &lt; &gt;</a>';
+      const markup = '<a href="a < b &quot; & c">& " &lt; &gt;</a>';
       expect(
-        html({ _useHtmlParser2: true, encodeEntities: "utf8" }, string_),
+        html({ _useHtmlParser2: true, encodeEntities: "utf8" }, markup),
       ).toStrictEqual('<a href="a < b &quot; &amp; c">&amp; " &lt; &gt;</a>');
     });
   });
@@ -65,30 +65,30 @@ describe("render DOM parsed with htmlparser2", () => {
 
   describe("(xml)", () => {
     it("should render CDATA correctly", () => {
-      const string_ =
+      const markup =
         "<a> <b> <![CDATA[ asdf&asdf ]]> <c/> <![CDATA[ asdf&asdf ]]> </b> </a>";
-      expect(xml(string_)).toStrictEqual(string_);
+      expect(xml(markup)).toStrictEqual(markup);
     });
 
     it('should append ="" to attributes with no value', () => {
-      const string_ = "<div dropdown-toggle>";
-      expect(xml(string_)).toStrictEqual('<div dropdown-toggle=""/>');
+      const markup = "<div dropdown-toggle>";
+      expect(xml(markup)).toStrictEqual('<div dropdown-toggle=""/>');
     });
 
     it('should append ="" to boolean attributes with no value', () => {
-      const string_ = "<input disabled>";
-      expect(xml(string_)).toStrictEqual('<input disabled=""/>');
+      const markup = "<input disabled>";
+      expect(xml(markup)).toStrictEqual('<input disabled=""/>');
     });
 
     it("should preserve XML prefixes on attributes", () => {
-      const string_ =
+      const markup =
         '<div xmlns:ex="http://example.com/ns"><p ex:ample="attribute">text</p></div>';
-      expect(xml(string_)).toStrictEqual(string_);
+      expect(xml(markup)).toStrictEqual(markup);
     });
 
     it("should preserve mixed-case XML elements and attributes", () => {
-      const string_ = '<svg viewBox="0 0 8 8"><radialGradient/></svg>';
-      expect(xml(string_)).toStrictEqual(string_);
+      const markup = '<svg viewBox="0 0 8 8"><radialGradient/></svg>';
+      expect(xml(markup)).toStrictEqual(markup);
     });
 
     it("should encode entities in otherwise special tags", () => {
@@ -98,16 +98,16 @@ describe("render DOM parsed with htmlparser2", () => {
     });
 
     it("should not encode entities if disabled", () => {
-      const string_ = '<script>"<br/>"</script>';
-      expect(xml(string_, { decodeEntities: false })).toStrictEqual(string_);
+      const markup = '<script>"<br/>"</script>';
+      expect(xml(markup, { decodeEntities: false })).toStrictEqual(markup);
     });
   });
 });
 
 describe("(xml, {selfClosingTags: false})", () => {
   it("should render childless nodes with an explicit closing tag", () => {
-    const string_ = "<foo /><bar></bar>";
-    expect(xml(string_, { selfClosingTags: false })).toStrictEqual(
+    const markup = "<foo /><bar></bar>";
+    expect(xml(markup, { selfClosingTags: false })).toStrictEqual(
       "<foo></foo><bar></bar>",
     );
   });
@@ -115,7 +115,7 @@ describe("(xml, {selfClosingTags: false})", () => {
 
 describe("(html, {selfClosingTags: true})", () => {
   it("should render <br /> tags correctly", () => {
-    const string_ = "<br />";
+    const markup = "<br />";
     expect(
       html(
         {
@@ -123,15 +123,15 @@ describe("(html, {selfClosingTags: true})", () => {
           decodeEntities: false,
           selfClosingTags: true,
         },
-        string_,
+        markup,
       ),
-    ).toStrictEqual(string_);
+    ).toStrictEqual(markup);
   });
 });
 
 describe("(html, {selfClosingTags: false})", () => {
   it("should render childless SVG nodes with an explicit closing tag", () => {
-    const string_ =
+    const markup =
       '<svg><circle x="12" y="12"></circle><path d="123M"></path><polygon points="60,20 100,40 100,80 60,100 20,80 20,40"></polygon></svg>';
     expect(
       html(
@@ -140,133 +140,133 @@ describe("(html, {selfClosingTags: false})", () => {
           decodeEntities: false,
           selfClosingTags: false,
         },
-        string_,
+        markup,
       ),
-    ).toStrictEqual(string_);
+    ).toStrictEqual(markup);
   });
 });
 
 function testBody(html: (input: string, options?: LoadingOptions) => string) {
   it("should render <br /> tags without a slash", () => {
-    const string_ = "<br />";
-    expect(html(string_)).toStrictEqual("<br>");
+    const markup = "<br />";
+    expect(html(markup)).toStrictEqual("<br>");
   });
 
   it("should retain encoded HTML content within attributes", () => {
-    const string_ = '<hr class="cheerio &amp; node = happy parsing" />';
-    expect(html(string_)).toStrictEqual(
+    const markup = '<hr class="cheerio &amp; node = happy parsing" />';
+    expect(html(markup)).toStrictEqual(
       '<hr class="cheerio &amp; node = happy parsing">',
     );
   });
 
   it('should shorten the "checked" attribute when it contains the value "checked"', () => {
-    const string_ = "<input checked/>";
-    expect(html(string_)).toStrictEqual("<input checked>");
+    const markup = "<input checked/>";
+    expect(html(markup)).toStrictEqual("<input checked>");
   });
 
   it("should render empty attributes if asked for", () => {
-    const string_ = "<input checked/>";
-    expect(html(string_, { emptyAttrs: true })).toStrictEqual(
+    const markup = "<input checked/>";
+    expect(html(markup, { emptyAttrs: true })).toStrictEqual(
       '<input checked="">',
     );
   });
 
   it('should not shorten the "name" attribute when it contains the value "name"', () => {
-    const string_ = '<input name="name"/>';
-    expect(html(string_)).toStrictEqual('<input name="name">');
+    const markup = '<input name="name"/>';
+    expect(html(markup)).toStrictEqual('<input name="name">');
   });
 
   it('should not append ="" to attributes with no value', () => {
-    const string_ = "<div dropdown-toggle>";
-    expect(html(string_)).toStrictEqual("<div dropdown-toggle></div>");
+    const markup = "<div dropdown-toggle>";
+    expect(html(markup)).toStrictEqual("<div dropdown-toggle></div>");
   });
 
   it("should render comments correctly", () => {
-    const string_ = "<!-- comment -->";
-    expect(html(string_)).toStrictEqual("<!-- comment -->");
+    const markup = "<!-- comment -->";
+    expect(html(markup)).toStrictEqual("<!-- comment -->");
   });
 
   it("should render whitespace by default", () => {
-    const string_ =
+    const markup =
       '<a href="./haha.html">hi</a> <a href="./blah.html">blah</a>';
-    expect(html(string_)).toStrictEqual(string_);
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should preserve multiple hyphens in data attributes", () => {
-    const string_ = '<div data-foo-bar-baz="value"></div>';
-    expect(html(string_)).toStrictEqual('<div data-foo-bar-baz="value"></div>');
+    const markup = '<div data-foo-bar-baz="value"></div>';
+    expect(html(markup)).toStrictEqual('<div data-foo-bar-baz="value"></div>');
   });
 
   it("should not encode characters in script tag", () => {
-    const string_ = '<script>alert("hello world")</script>';
-    expect(html(string_)).toStrictEqual(string_);
+    const markup = '<script>alert("hello world")</script>';
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should not encode tags in script tag", () => {
-    const string_ = '<script>"<br>"</script>';
-    expect(html(string_)).toStrictEqual(string_);
+    const markup = '<script>"<br>"</script>';
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should not encode json data", () => {
-    const string_ =
+    const markup =
       '<script>var json = {"simple_value": "value", "value_with_tokens": "&quot;here & \'there\'&quot;"};</script>';
-    expect(html(string_)).toStrictEqual(string_);
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should render childless SVG nodes with a closing slash in HTML mode", () => {
-    const string_ =
+    const markup =
       '<svg><circle x="12" y="12"/><path d="123M"/><polygon points="60,20 100,40 100,80 60,100 20,80 20,40"/></svg>';
-    expect(html(string_)).toStrictEqual(string_);
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should render childless MathML nodes with a closing slash in HTML mode", () => {
-    const string_ = "<math><infinity/></math>";
-    expect(html(string_)).toStrictEqual(string_);
+    const markup = "<math><infinity/></math>";
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should allow SVG elements to have children", () => {
-    const string_ =
+    const markup =
       '<svg><circle cx="12" r="12"><title>dot</title></circle></svg>';
-    expect(html(string_)).toStrictEqual(string_);
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should not include extra whitespace in SVG self-closed elements", () => {
-    const string_ = '<svg><image href="x.png"/>     </svg>';
-    expect(html(string_)).toStrictEqual(string_);
+    const markup = '<svg><image href="x.png"/>     </svg>';
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should fix-up bad nesting in SVG in HTML mode", () => {
-    const string_ = '<svg><g><image href="x.png"></svg>';
-    expect(html(string_)).toStrictEqual(
+    const markup = '<svg><g><image href="x.png"></svg>';
+    expect(html(markup)).toStrictEqual(
       '<svg><g><image href="x.png"/></g></svg>',
     );
   });
 
   it("should preserve XML prefixed attributes on inline SVG nodes in HTML mode", () => {
-    const string_ =
+    const markup =
       '<svg><text id="t" xml:lang="fr">Bonjour</text><use xlink:href="#t"/></svg>';
-    expect(html(string_)).toStrictEqual(string_);
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should handle mixed-case SVG content in HTML mode", () => {
-    const string_ = '<svg viewBox="0 0 8 8"><radialGradient/></svg>';
-    expect(html(string_)).toStrictEqual(string_);
+    const markup = '<svg viewBox="0 0 8 8"><radialGradient/></svg>';
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should render HTML content in SVG foreignObject in HTML mode", () => {
-    const string_ =
+    const markup =
       '<svg><foreignObject requiredFeatures=""><img src="test.png" viewbox>text<svg viewBox="0 0 8 8"><circle r="3"/></svg></foreignObject></svg>';
-    expect(html(string_)).toStrictEqual(string_);
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should render iframe nodes with a closing tag in HTML mode", () => {
-    const string_ = '<iframe src="test"></iframe>';
-    expect(html(string_)).toStrictEqual(string_);
+    const markup = '<iframe src="test"></iframe>';
+    expect(html(markup)).toStrictEqual(markup);
   });
 
   it("should encode double quotes in attribute", () => {
-    const string_ = `<img src="/" alt='title" onerror="alert(1)" label="x'>`;
-    expect(html(string_)).toStrictEqual(
+    const markup = `<img src="/" alt='title" onerror="alert(1)" label="x'>`;
+    expect(html(markup)).toStrictEqual(
       '<img src="/" alt="title&quot; onerror=&quot;alert(1)&quot; label=&quot;x">',
     );
   });
